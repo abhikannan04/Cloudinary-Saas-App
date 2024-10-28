7ba87e65ac0b94018fcc7e0ed86632a49af58c29
Abhilash Kannan <abhikannan200304@gmail.com>
2024-10-28 11:46:26 +0530

    Added all the files

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,14 @@
+import { SignIn } from '@clerk/nextjs';
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+        <h1 className="text-2xl font-semibold text-center mb-6 text-gray-800">
+          Welcome Back
+        </h1>
+        <SignIn />
+      </div>
+    </div>
+  );
+}
